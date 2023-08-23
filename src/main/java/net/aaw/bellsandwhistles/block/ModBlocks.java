403116cc4e3d417666c594d3a53fa3e878cab561
levtus <61996958+alexandsr@users.noreply.github.com>
2023-08-23 16:20:18 +0200

    Add more pilot types

--- src/main/java/net/aaw/bellsandwhistles/block/ModBlocks.java
+++ src/main/java/net/aaw/bellsandwhistles/block/ModBlocks.java
@@ -26,8 +26,6 @@ public class ModBlocks {
     }
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, BellsAndWhistles.MOD_ID);
-    public static final RegistryObject<Block> METAL_PILOT = registerBlock("metal_pilot",
-            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
     public static final RegistryObject<Block> ANDESITE_GRAB_RAILS = registerBlock("andesite_grab_rails",
             () -> new MetalGrabRailsBlock(BlockBehaviour.Properties.copy(Blocks.LADDER).sound(SoundType.METAL).noOcclusion()));
     public static final RegistryObject<Block> BRASS_GRAB_RAILS = registerBlock("brass_grab_rails",
@@ -42,6 +40,42 @@ public class ModBlocks {
     public static final RegistryObject<Block> COPPER_BOGIE_STEPS = registerBlock("copper_bogie_steps",
             () -> new MetalBogieStepsBlock(BlockBehaviour.Properties.copy(Blocks.LADDER).sound(SoundType.METAL).noOcclusion()));
 
+    public static final RegistryObject<Block> METAL_PILOT = registerBlock("metal_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> ANDESITE_PILOT = registerBlock("andesite_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> BRASS_PILOT = registerBlock("brass_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> COPPER_PILOT = registerBlock("copper_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_ANDESITE_PILOT = registerBlock("polished_andesite_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_ASURINE_PILOT = registerBlock("polished_asurine_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_CALCITE_PILOT = registerBlock("polished_calcite_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_CRIMSITE_PILOT = registerBlock("polished_crimsite_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_DEEPSLATE_PILOT = registerBlock("polished_deepslate_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_DIORITE_PILOT = registerBlock("polished_diorite_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_DRIPSTONE_PILOT = registerBlock("polished_dripstone_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_GRANITE_PILOT = registerBlock("polished_granite_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_LIMESTONE_PILOT = registerBlock("polished_limestone_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_OCHRUM_PILOT = registerBlock("polished_ochrum_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_SCORCHIA_PILOT = registerBlock("polished_scorchia_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_SCORIA_PILOT = registerBlock("polished_scoria_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_TUFF_PILOT = registerBlock("polished_tuff_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+    public static final RegistryObject<Block> POLISHED_VERIDIUM_PILOT = registerBlock("polished_veridium_pilot",
+            () -> new PilotBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
 
     private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
--- src/main/java/net/aaw/bellsandwhistles/item/ModCreativeModeTabs.java
+++ src/main/java/net/aaw/bellsandwhistles/item/ModCreativeModeTabs.java
@@ -21,14 +21,31 @@ public class ModCreativeModeTabs {
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(ModItems.WOODEN_WHISTLE.get());
                         pOutput.accept(ModItems.BRASS_WHISTLE.get());
-                        pOutput.accept(ModBlocks.METAL_PILOT.get());
+                        pOutput.accept(ModItems.EVANGELION_DISC.get());
                         pOutput.accept(ModBlocks.ANDESITE_GRAB_RAILS.get());
                         pOutput.accept(ModBlocks.BRASS_GRAB_RAILS.get());
                         pOutput.accept(ModBlocks.COPPER_GRAB_RAILS.get());
                         pOutput.accept(ModBlocks.ANDESITE_BOGIE_STEPS.get());
                         pOutput.accept(ModBlocks.BRASS_BOGIE_STEPS.get());
                         pOutput.accept(ModBlocks.COPPER_BOGIE_STEPS.get());
-                        pOutput.accept(ModItems.EVANGELION_DISC.get());
+                        pOutput.accept(ModBlocks.METAL_PILOT.get());
+                        pOutput.accept(ModBlocks.ANDESITE_PILOT.get());
+                        pOutput.accept(ModBlocks.BRASS_PILOT.get());
+                        pOutput.accept(ModBlocks.COPPER_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_ANDESITE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_GRANITE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_DIORITE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_DEEPSLATE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_DRIPSTONE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_TUFF_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_CALCITE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_LIMESTONE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_SCORIA_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_SCORCHIA_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_CRIMSITE_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_OCHRUM_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_VERIDIUM_PILOT.get());
+                        pOutput.accept(ModBlocks.POLISHED_ASURINE_PILOT.get());
                     })
                     .build());
 
