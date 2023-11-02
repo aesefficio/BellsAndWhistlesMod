d476ab1f81e83a55f2a20aba0cc05b01f60994be
levtus <i@alexander.systems>
2023-11-02 09:34:01 -0500

    Add 1.19.2 MDK

---
+++ src/main/java/com/example/modid/ExampleMod.java
@@ -0,0 +1,30 @@
+package com.example.modid;
+
+import com.simibubi.create.Create;
+
+import io.github.fabricators_of_create.porting_lib.util.EnvExecutor;
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.ResourceLocation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+	public static final String ID = "modid";
+	public static final String NAME = "Example Mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Create addon mod [{}] is loading alongside Create [{}]!", NAME, Create.VERSION);
+		LOGGER.info(EnvExecutor.unsafeRunForDist(
+				() -> () -> "{} is accessing Porting Lib from the client!",
+				() -> () -> "{} is accessing Porting Lib from the server!"
+		), NAME);
+	}
+
+	public static ResourceLocation id(String path) {
+		return new ResourceLocation(ID, path);
+	}
+}
---
+++ src/main/java/com/example/modid/mixin/MinecraftMixin.java
@@ -0,0 +1,20 @@
+package com.example.modid.mixin;
+
+import com.example.modid.ExampleMod;
+
+import net.minecraft.client.Minecraft;
+
+import net.minecraft.client.main.GameConfig;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void example$init(GameConfig gameConfig, CallbackInfo ci) {
+		ExampleMod.LOGGER.info("Hello from {}", ExampleMod.NAME);
+	}
+}
